b9c0f66f97c0b286757accd0d60493afec039050
BD512 <bethd835@gmail.com>
2026-04-30 15:23:20 +0100

    Just rough code for the add record GUI - I'll finish it later.

--- AddInfoGui/RecordDetailsEntry.py
+++ AddInfoGui/RecordDetailsEntry.py
@@ -4,4 +4,9 @@ class RecordDetailsEntry(Toplevel):
     def __init__(self, master):
         super().__init__(master)
         Label(self, text="Record Details").grid(row=0, column=0, sticky=W)
-        Label(self, text="Item name").grid(row)
+        Label(self, text="Item name").grid(row=1, column=0, sticky=W)
+        name_entry = Entry(self)
+        name_entry.grid(row=1, column=1)
+        Label(self, text="Item price").grid(row=2, column=0, sticky=W)
+        price_entry = Entry(self)
+        price_entry.grid(row=2, column=1)
