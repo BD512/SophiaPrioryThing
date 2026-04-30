66f278ceb14bdb0cdcd38af0321e661e0cbfdf95
BD512 <bethd835@gmail.com>
2026-04-30 14:38:34 +0100

    Saved create database code.

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,7 +1,14 @@
-CREATE Table HistoricalItems (
-                                 IDNumber INT NOT NULL PRIMARY KEY,
+
+CREATE TABLE HistoricalItems (
+                                 IDNumber INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                  Name VARCHAR(50) NOT NULL,
+                                 Category VARCHAR(20) NOT NULL,
                                  Description VARCHAR(1000),
-                                 Date DATE,
-                                 ImagePath VARCHAR(200) NOT NULL
+                                 Date DATE
+);
+CREATE TABLE ItemsImages (
+                             IDNumber INTEGER NOT NULL,
+                             ImagePath VARCHAR(100) NOT NULL,
+                             FOREIGN KEY (IDNumber) REFERENCES HistoricalItems(IDNumber),
+                             PRIMARY KEY (IDNumber, ImagePath)
 );
