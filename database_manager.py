5b094708f32016abe8c3b913aab40e0f8014c1d5
k <itskaela08@gmail.com>
2026-05-02 13:13:19 +0100

    comment

--- database_manager.py
+++ database_manager.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     d = DatabaseManager()
     # testing
     # d.insert_into_item(name="Cross",description="Very nice.",category="Crucifixes",year=2000)
-    d.insert_into_category("MISC","M")
+    # d.insert_into_category("MISC","M")
     # print(d.get_category("Lecturns"))
     # print(d.get_subcategories("CS"))
     # print(d.get_category_dict(("S", "L", "CS", "P", "W", "E", "LC", "M")))
