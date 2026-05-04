1f17b9f9f86e5299f214b1936ba0f155652451c0
BD512 <bethd835@gmail.com>
2026-05-04 20:21:43 +0100

    Made some changes to the layout of the entry window.

--- RecordDetailsEntryv2.py
+++ RecordDetailsEntryv2.py
@@ -9,92 +9,94 @@ from database_manager import DatabaseManager
 class RecordEntryGUI(Toplevel):
     def __init__(self, master):
         super().__init__(master)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+        self.grid_columnconfigure((0, 1, 2), weight=1)
 
         self.database = DatabaseManager()
 
         self.confidence_level = IntVar()
         self.categories = ("S", "L", "CS", "P", "W", "E", "LC", "M")  # set categories
 
-        Label(self, text="Item name:").grid(row=0, column=0, padx=10, pady=(5, 0))
+        Label(self, text="Item name:").grid(row=0, column=0, padx=10, pady=(5, 0), sticky="nsew")
         self.name_entry = Entry(self)
-        self.name_entry.grid(row=1, column=0, padx=10, pady=10)
+        self.name_entry.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
-        Label(self, text="Category:").grid(row=0, column=1, padx=10, pady=(5, 0))
+        Label(self, text="Category:").grid(row=1, column=0, padx=10, pady=(5, 0), sticky="nsew")
         self.category_menu = ttk.Combobox(self, values=self.categories)
-        self.category_menu.grid(row=1, column=1, padx=10, pady=0)
-        self.category_menu.bind('<<ComboboxSelected>>', self.update_subcategory_menu)
+        self.category_menu.grid(row=1, column=1, padx=10, pady=0, sticky="nsew")
+        self.category_menu.bind('<<ComboboxSelected>>', self.updateSubcategoryMenu)
         self.category_menu.set("M")
 
         self.category_dict = self.database.get_category_dict(self.categories)
         # print(self.category_dict)
         # print(self.category_dict["M"])
 
-        Label(self, text="Subcategory:").grid(row=2, column=1, padx=10, pady=0)
+        Label(self, text="Subcategory:").grid(row=2, column=0, padx=10, pady=0, sticky="nsew")
         self.subcategory_menu = ttk.Combobox(self, values=self.category_dict["M"])  # default selection is miscellaneous
-        self.subcategory_menu.grid(row=3, column=1, padx=10, pady=(0, 10))
-        self.subcategory_menu.bind('<FocusIn>', self.update_subcategory_menu)
-        self.subcategory_menu.bind('<Button-1>', self.update_subcategory_menu)
+        self.subcategory_menu.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+        self.subcategory_menu.bind('<FocusIn>', self.updateSubcategoryMenu)
+        self.subcategory_menu.bind('<Button-1>', self.updateSubcategoryMenu)
 
-        Label(self, text="Description:").grid(row=0, column=2, padx=10, pady=(5, 0))
-        self.description_entry = Text(self, height=5, width=25, wrap="word")
-        self.description_entry.grid(row=1, column=2, rowspan=3, pady=10, sticky="n")
+        Label(self, text="Description:").grid(row=3, column=0, padx=10, pady=(5, 0), sticky="nsew")
+        self.description_entry = Text(self, wrap="word", width=4, height=4)
+        self.description_entry.grid(row=3, column=1, pady=10, sticky="nsew")
 
-        Label(self, text="Year:").grid(row=0, column=3, padx=10, pady=(5, 0))
+        Label(self, text="Year:").grid(row=4, column=0, padx=10, pady=(5, 0))
         self.year_entry = Entry(self)
-        self.year_entry.grid(row=1, column=3, padx=10, pady=0)
+        self.year_entry.grid(row=4, column=1, padx=10, pady=0)
 
         # Label(self, text="Year confidence:").grid(row=0, column=4, padx=(5,0), pady=10)
-        self.confident_check = ttk.Checkbutton(self, text="I am confident", variable=self.confidence_level, onvalue=1,
+        self.confident_check = ttk.Checkbutton(self, text="I am confident in year", variable=self.confidence_level, onvalue=1,
                                                offvalue=0)
-        self.confident_check.grid(row=2, column=3, padx=10, pady=10, sticky="n")
+        self.confident_check.grid(row=5, column=1, padx=10, pady=10, sticky="nsew")
 
-        addBtn = Button(self, text="Add item")
-        addBtn.grid(row=4, column=3, padx=10, pady=10, sticky="n")
+        addBtn = Button(self, text="Add item") # the final submit button
+        addBtn.grid(row=6, column=1, padx=10, pady=10, sticky="nsew")
 
-        testBtn = Button(self, text="test", command=self.test)
-        testBtn.grid(row=2, column=0)
+        # testBtn = Button(self, text="test", command=self.test)
+        # testBtn.grid(row=6, column=1)
 
-    def update_subcategory_menu(self, event):
+    def updateSubcategoryMenu(self, event):
         current_category = self.category_menu.get()
         self.subcategory_menu.config(values=self.category_dict[current_category])
 
-    def update_error_msg(self):
+    def updateErrorMsg(self):
         pass
 
-    def get_name(self) -> str:
+    def getName(self) -> str:
         return self.name_entry.get()
 
-    def get_subcategory(self) -> str:
+    def getSubcategory(self) -> str:
         return self.subcategory_menu.get()
 
-    def get_description(self) -> str:
+    def getDescription(self) -> str:
         return self.description_entry.get("1.0", "end-1c")
 
-    def get_year(self) -> int:
+    def getYear(self) -> int:
         return int(self.year_entry.get())
 
-    def get_confidence(self) -> bool:
+    def getConfidence(self) -> bool:
         return bool(self.confidence_level.get())
 
-    def get_item_details(self) -> tuple[str, str, str, int, bool]:
-        return self.get_name(), self.get_subcategory(), self.get_description(), self.get_year(), self.get_confidence()
+    def getItemDetails(self) -> tuple[str, str, str, int, bool]:
+        return self.getName(), self.getSubcategory(), self.getDescription(), self.getYear(), self.getConfidence()
 
-    def is_valid_record(self) -> bool:
+    def isValidRecord(self) -> bool:
         pass
 
-    def is_new_category(self):
+    def isNewCategory(self):
         pass
 
-    def is_new_subcategory(self):
+    def isNewSubcategory(self):
         pass
 
     def test(self):
-        print(self.get_name(), self.get_subcategory(), self.get_description(), self.get_year(), self.get_confidence())
+        print(self.getName(), self.getSubcategory(), self.getDescription(), self.getYear(), self.getConfidence())
         # print(bool(self.get_name()))
 
-    def add_item_record(self):
+    def addItemRecord(self):
 
-        current_item_details = self.get_item_details()
+        current_item_details = self.getItemDetails()
         self.database.insert_into_item(current_item_details)
 
         try:
