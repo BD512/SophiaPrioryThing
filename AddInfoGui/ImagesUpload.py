a485d7714a1034300a082a47e6618b626c4c84c0
BD512 <bethd835@gmail.com>
2026-05-07 21:41:07 +0100

    Finished code to list the image paths which will go in the entry window.

--- AddInfoGui/ImagesUpload.py
+++ AddInfoGui/ImagesUpload.py
@@ -1,5 +1,5 @@
-from tkinter import *
-from tkinter import filedialog as fd
+from tkinter import filedialog, Tk, Button, Frame, StringVar, Entry, Canvas, Scrollbar, Menu
+from tkinter.ttk import Treeview
 import os
 
 class FileUpload(Frame):
@@ -16,7 +16,7 @@ class FileUpload(Frame):
 
     def selectFilePath(self) -> str:
         filetypes = self.file_types
-        f = fd.askopenfile(filetypes=filetypes, initialdir=self.path)
+        f = filedialog.askopenfile(filetypes=filetypes, initialdir=self.path)
         print(self.path)
         if f:
             filepath = os.path.abspath(f.name)
@@ -38,8 +38,53 @@ class FileUpload(Frame):
     def getPath(self) -> str:
         return self.path
 
-class FilesUploadList(Frame):
-    pass
+# todo add validation so can't upload the same image twice
+class FilesUploadList(Treeview):
+    def __init__(self, master, image_paths: tuple):
+        super().__init__(master)
+        self.image_paths: list = list(image_paths)
+        self.column("#1")
+        self.heading("#1", text="Image path")
+        self.right_click_options = Menu(self, tearoff=0)  ######## self might have to be a Tk instance??
+        self.right_click_options.add_command(label="Delete", command=self.deleteSelection)
+        self.bind("<Button-3>", self.showRightClickOptions)
+
+    def getSelection(self):
+        return self.selection()[0]
+
+    def deleteSelection(self):
+        path = self.getSelection()
+        self.deletePath(path)
+
+    def deletePath(self, path: str):
+        if path in self.image_paths:
+            self.image_paths.remove(path)
+        self.updatePaths()
+
+    def showPaths(self):
+        for path in self.image_paths:
+            self.showImageFilePath(path)
+
+    def updatePaths(self):
+        self.clear()
+        self.showPaths()
+
+    def clear(self):
+        for child in self.get_children():
+            self.delete(child)
+
+    def showRightClickOptions(self, event):
+        row_id = self.identify_row(event.y)
+        if row_id is not None:
+            self.selection_set(row_id)
+            self.right_click_options.post(event.x_root, event.y_root)
+
+    def addImageFilePath(self, path):
+        self.image_paths.append(path)
+        self.showImageFilePath(path)
+
+    def showImageFilePath(self, path: str):
+        self.insert('', "end", iid=path, values=(path, ))
 
 class FilesUpload(Canvas):
     def __init__(self, master):
@@ -49,6 +94,8 @@ class FilesUpload(Canvas):
         self.scrollbar.pack(side="right", fill="y")
         self.configure(yscrollcommand=self.scrollbar.set)
 
-    def 
 
 # todo - once the file paths have been selected, move to single folder
+a = Tk()
+FileUpload(a).pack()
+a.mainloop()
