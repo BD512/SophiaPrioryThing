ce2bfb8d478352619a6b93f3042d104e15c5eed5
BD512 <bethd835@gmail.com>
2026-05-10 19:50:08 +0100

    Added methods for adding images to database.

--- database_manager.py
+++ database_manager.py
@@ -12,12 +12,12 @@ class DatabaseManager:
         self.cursor = self.conn.cursor()
         self.create_database() # created the tables in the database if they don't exist
         # it would be better programming if we got the below attributes from get_categories_dict
-        self.categories = ("Stalls","Liturgical Items","Crosses and Staves","Pulpit and Lecturn","Stained Glass Windows",\
+        self.categories = ("Stalls","Liturgical Items","Crosses and Staves","Pulpit and Lecturn","Stained Glass Windows",
                            "Embroidery","Lighting and Candles","Miscellaneous")
-        self.subcategories = ("Ancient Stalls","Bishop Stalls","Sedilia","Chalice","Ciborium","Collection Plate","Flagon",\
-                              "Paten","Thurible","Coptic Crosses","Processional Crosses","Crucifixes","Churchwarden Staves",\
-                              "Verges","Pulpit","Lecturn","Regimental Chapel","Main Church","Altar Frontals - High Altar",\
-                              "Altar Frontals - Other","Hassocks","Copes","Chasubles","Chandeliers","Altar Candles",\
+        self.subcategories = ("Ancient Stalls","Bishop Stalls","Sedilia","Chalice","Ciborium","Collection Plate","Flagon",
+                              "Paten","Thurible","Coptic Crosses","Processional Crosses","Crucifixes","Churchwarden Staves",
+                              "Verges","Pulpit","Lecturn","Regimental Chapel","Main Church","Altar Frontals - High Altar",
+                              "Altar Frontals - Other","Hassocks","Copes","Chasubles","Chandeliers","Altar Candles",
                               "Baptismal Fonts","Icons","Statues","Wooden Chests","Aumbry")
 
     # method that creates database and tables needed from scratch
@@ -75,7 +75,7 @@ class DatabaseManager:
         self.conn.commit() # updates changes
 
     # method to insert a new record into historic items table
-    def insert_into_item(self,name:str|None=None,subcategory="MISC",description:str|None = None,year=-1,confidence=0):
+    def insert_into_item(self,name:str|None=None,subcategory="MISC",description:str|None = None,year=-1,confidence=0, images: list=None):
         if year < 0: year = None # years in AD must be positive
         if description == "": description = None
         # temp = [name,subcategory,description]
@@ -91,16 +91,28 @@ class DatabaseManager:
                             VALUES (?, ?, ?, ?, ?);", (name, subcategory, description, year, confidence))
         # print("should be adding it tp the table?")
         self.conn.commit() # updates changes
+        if images is not None:
+            self.add_images_for_item(self.cursor.lastrowid, images)
 
-    def edit_item_record(self,item_id,name:str|None=None,subcategory="MISC",description:str|None = None,year=-1,confidence=0):
+    def edit_item_record(self,item_id,name:str|None=None,subcategory="MISC",description:str|None = None,year=-1,confidence=0, images: list=None):
         if year < 0: year = None # years in AD must be positive
         if description == "": description = None
         
         print(f"{name},{subcategory},{description},{year},{confidence}")
         self.cursor.execute(f"UPDATE {self.item_table} SET Name=?, Subcategory=?, Description=?, Year=?, Confidence=? \
-                            WHERE IDNumber=?);", (name,subcategory,description,year,confidence,),(item_id,))
-
+                            WHERE IDNumber=?);", (name,subcategory,description,year,confidence,item_id,),)
         self.conn.commit() # updates changes
+        self.delete_images_for_item(item_id)
+        if images is not None: self.add_images_for_item(item_id, images)
+
+
+    def delete_images_for_item(self, id_number):
+        self.cursor.execute(f"DELETE FROM {self.image_table} WHERE IDNumber=?;", (id_number,))
+        self.conn.commit()
+
+    def add_images_for_item(self, id_number: int, images: list):
+        for image in images:
+            self.insert_into_image(id_number, image)
 
     # method to insert a new record into matching category table
     def insert_into_category(self,subcategory="MISC",category="MISC",description:str|None = None):
@@ -250,19 +262,19 @@ class DatabaseManager:
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Paten","Liturgical Items"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Thurible","Liturgical Items"))
         # category CS
-        self.create_subcategory("Coptic Crosses","Crosses and Staves")
-        self.create_subcategory("Processional Crosses","Crosses and Staves")
-        self.create_subcategory("Crucifixes","Crosses and Staves")
-        self.create_subcategory("Churchwarden Staves","Crosses and Staves")
-        self.create_subcategory("Verges","Crosses and Staves")
+        self.create_subcategory("Coptic Crosses","Crosses And Staves")
+        self.create_subcategory("Processional Crosses","Crosses And Staves")
+        self.create_subcategory("Crucifixes","Crosses And Staves")
+        self.create_subcategory("Churchwarden Staves","Crosses And Staves")
+        self.create_subcategory("Verges","Crosses And Staves")
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Coptic Crosses","Crosses and Staves"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Processional Crosses","Crosses and Staves"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Crucifixes","Crosses and Staves"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Churchwarden Staves","Crosses and Staves"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Verges","Crosses and Staves"))
         # category P
-        self.create_subcategory("Pulpit","Pulpit and Lecturn")
-        self.create_subcategory("Lecturn","Pulpit and Lecturn")
+        self.create_subcategory("Pulpit","Pulpit And Lecturn")
+        self.create_subcategory("Lecturn","Pulpit And Lecturn")
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Pulpit","Pulpit and Lecturn"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Lecturn","Pulpit and Lecturn"))
         # category W
@@ -282,8 +294,8 @@ class DatabaseManager:
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Copes","Embroidery"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Chasubles","Embroidery"))
         # category LC
-        self.create_subcategory("Chandeliers","Lighting and Candles")
-        self.create_subcategory("Altar Candles","Lighting and Candles")
+        self.create_subcategory("Chandeliers","Lighting And Candles")
+        self.create_subcategory("Altar Candles","Lighting And Candles")
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Chandeliers","Lighting and Candles"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Altar Candles","Lighting and Candles"))
         # category M
@@ -298,8 +310,8 @@ class DatabaseManager:
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Wooden Chests","Miscellaneous"))
         # self.cursor.execute(f"INSERT INTO {self.category_table} (Subcategory,Category) VALUES (?,?);",("Aumbry","Miscellaneous"))
 
-    def delete_record(self,id):
-        self.cursor.execute(f"DELETE FROM {self.item_table} WHERE IDNumber =?",(id,))
+    def delete_record(self,identifier):
+        self.cursor.execute(f"DELETE FROM {self.item_table} WHERE IDNumber =?", (identifier,))
         self.conn.commit()
 
     # method that commits changes and closes connection before a table object is garbage-collected
